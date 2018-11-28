edb0d3d0390f307e244b7997c7bdc1713183eb48
Unknown <zeleng_han@mail.ru>
2018-11-28 19:32:56 +0300

    check test

---
+++ tests/CheckTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* This file is part of the komtet/kassa-sdk library
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace KomtetTest\KassaSdk;
+
+use Komtet\KassaSdk\Check;
+use Komtet\KassaSdk\Payment;
+use Komtet\KassaSdk\Position;
+use Komtet\KassaSdk\Vat;
+
+class CheckTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->check = new Check('id1', 'test@test.test', Check::INTENT_SELL, 1);
+    }
+
+    public function testApplyDiscount()
+    {
+        $vat = new Vat(0);
+        $payment1 = new Payment(Payment::TYPE_CARD, 120.0);
+        $payment2 = new Payment(Payment::TYPE_CARD, 15.0);
+        $position1 = new Position('position1', 100.0, 1, 100.0, 0, $vat);
+        $position2 = new Position('position2', 25.0, 2, 50.0, 0, $vat);
+
+        $this->check->addPayment($payment1);
+        $this->check->addPayment($payment2);
+        $this->check->addPosition($position1);
+        $this->check->addPosition($position2);
+        $discount = 15.0;
+
+        $this->check->applyDiscount($discount);
+
+        $positionsTotal = 0;
+        foreach( $this->check->positions as $position )
+        {
+            $positionsTotal += $position->total;
+        }
+
+        $this->assertEquals($positionsTotal, $this->check->getTotalPaymentsSum()-$discount);
+    }
+}
