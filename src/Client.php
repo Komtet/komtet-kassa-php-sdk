0ed5fa43ce4633aae14b0c50d5c8596390ef9867
awekening2 <alexandr.motmom@gmail.com>
2019-09-03 15:16:02 +0300

    поднятие версии

--- src/Client.php
+++ src/Client.php
@@ -95,7 +95,7 @@ class Client
             $system_php_serialize_precision = ini_get('serialize_precision');
             $system_php_precision = ini_get('precision');
 
-            if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+            if (version_compare(PHP_VERSION, '7.1.0') >= 0) {
                 ini_set('precision', 10);
                 ini_set('serialize_precision', 10);
             }
