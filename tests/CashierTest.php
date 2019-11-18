97e8ac9f66e33f5a335e27389f82c4c0255fc578
awekening2 <alexandr.motmom@gmail.com>
2019-11-18 18:31:05 +0300

    inn необязателен

---
+++ tests/CashierTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+* This file is part of the komtet/kassa-sdk library
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace KomtetTest\KassaSdk;
+
+use Komtet\KassaSdk\AuthorisedPerson;
+use Komtet\KassaSdk\Cashier;
+
+class CashierTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCashierNameWithInn()
+    {
+        $cashier = new Cashier('Иваров И.П.', '1234567890123');
+        $this->assertEquals($cashier->asArray()['name'], 'Иваров И.П.');
+        $this->assertEquals($cashier->asArray()['inn'], '1234567890123');
+    }
+
+    public function testCashierNameWithoutInn()
+    {
+        $cashier = new Cashier('Иваров И.П.');
+        $this->assertEquals($cashier->asArray()['name'], 'Иваров И.П.');
+        $this->assertEmpty($cashier->asArray()['inn']);
+    }
+
+    public function testAuthorisedPersonNameWithInn()
+    {
+        $cashier = new AuthorisedPerson('Иваров И.П.', '1234567890123');
+        $this->assertEquals($cashier->asArray()['name'], 'Иваров И.П.');
+        $this->assertEquals($cashier->asArray()['inn'], '1234567890123');
+    }
+
+    public function testAuthorisedPersonNameWithoutInn()
+    {
+        $cashier = new AuthorisedPerson('Иваров И.П.');
+        $this->assertEquals($cashier->asArray()['name'], 'Иваров И.П.');
+        $this->assertEmpty($cashier->asArray()['inn']);
+    }
+}
+
+
